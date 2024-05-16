ce7e5a1f935c276aad9462726cb5dcc9e23bef1f
Shriharsh <deshmukhshriharsh7@gmail.com>
2024-05-16 21:01:13 +0530

    Update V2.0(27.44)

--- Application/EdTech/EdTech/Form1.Designer.vb
+++ Application/EdTech/EdTech/Form1.Designer.vb
@@ -24,20 +24,22 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Panel1 = New Panel()
-        PictureBox1 = New PictureBox()
-        Panel2 = New Panel()
-        textIDNumber = New TextBox()
+        Label2 = New Label()
+        Label1 = New Label()
+        btnLogin = New Button()
         Panel3 = New Panel()
         txtPassword = New TextBox()
-        Button1 = New Button()
-        Label1 = New Label()
-        Label2 = New Label()
+        Panel2 = New Panel()
+        textIDNumber = New TextBox()
+        PictureBox1 = New PictureBox()
         Label3 = New Label()
-        Label4 = New Label()
+        Label5 = New Label()
+        TextBox1 = New TextBox()
+        Button2 = New Button()
         Panel1.SuspendLayout()
-        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
-        Panel2.SuspendLayout()
         Panel3.SuspendLayout()
+        Panel2.SuspendLayout()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' Panel1
@@ -45,7 +47,7 @@ Partial Class Form1
         Panel1.BackColor = Color.FromArgb(CByte(161), CByte(0), CByte(0))
         Panel1.Controls.Add(Label2)
         Panel1.Controls.Add(Label1)
-        Panel1.Controls.Add(Button1)
+        Panel1.Controls.Add(btnLogin)
         Panel1.Controls.Add(Panel3)
         Panel1.Controls.Add(Panel2)
         Panel1.Dock = DockStyle.Right
@@ -54,39 +56,47 @@ Partial Class Form1
         Panel1.Size = New Size(341, 486)
         Panel1.TabIndex = 0
         ' 
-        ' PictureBox1
+        ' Label2
         ' 
-        PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), Image)
-        PictureBox1.BackgroundImageLayout = ImageLayout.None
-        PictureBox1.Location = New Point(-19, 48)
-        PictureBox1.Name = "PictureBox1"
-        PictureBox1.Size = New Size(233, 55)
-        PictureBox1.TabIndex = 1
-        PictureBox1.TabStop = False
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label2.ForeColor = Color.White
+        Label2.Location = New Point(50, 136)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(195, 20)
+        Label2.TabIndex = 5
+        Label2.Text = "Or login with your account"
         ' 
-        ' Panel2
+        ' Label1
         ' 
-        Panel2.BackColor = Color.White
-        Panel2.Controls.Add(textIDNumber)
-        Panel2.Location = New Point(50, 150)
-        Panel2.Name = "Panel2"
-        Panel2.Size = New Size(248, 31)
-        Panel2.TabIndex = 2
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label1.ForeColor = Color.White
+        Label1.Location = New Point(185, 270)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(113, 17)
+        Label1.TabIndex = 4
+        Label1.Text = "Forget password?"
         ' 
-        ' textIDNumber
+        ' btnLogin
         ' 
-        textIDNumber.BorderStyle = BorderStyle.None
-        textIDNumber.Font = New Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        textIDNumber.Location = New Point(10, 5)
-        textIDNumber.Name = "textIDNumber"
-        textIDNumber.Size = New Size(230, 22)
-        textIDNumber.TabIndex = 2
+        btnLogin.BackColor = Color.White
+        btnLogin.FlatAppearance.BorderSize = 0
+        btnLogin.FlatStyle = FlatStyle.Flat
+        btnLogin.Font = New Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnLogin.ForeColor = Color.FromArgb(CByte(161), CByte(0), CByte(0))
+        btnLogin.Location = New Point(52, 264)
+        btnLogin.Name = "btnLogin"
+        btnLogin.Size = New Size(75, 29)
+        btnLogin.TabIndex = 2
+        btnLogin.Text = "LOGIN"
+        btnLogin.UseVisualStyleBackColor = False
         ' 
         ' Panel3
         ' 
         Panel3.BackColor = Color.White
         Panel3.Controls.Add(txtPassword)
-        Panel3.Location = New Point(50, 194)
+        Panel3.Location = New Point(50, 212)
         Panel3.Name = "Panel3"
         Panel3.Size = New Size(248, 31)
         Panel3.TabIndex = 3
@@ -100,63 +110,82 @@ Partial Class Form1
         txtPassword.Size = New Size(230, 22)
         txtPassword.TabIndex = 2
         ' 
-        ' Button1
+        ' Panel2
         ' 
-        Button1.BackColor = Color.White
-        Button1.FlatAppearance.BorderSize = 0
-        Button1.FlatStyle = FlatStyle.Flat
-        Button1.Font = New Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        Button1.ForeColor = Color.FromArgb(CByte(161), CByte(0), CByte(0))
-        Button1.Location = New Point(52, 246)
-        Button1.Name = "Button1"
-        Button1.Size = New Size(75, 29)
-        Button1.TabIndex = 2
-        Button1.Text = "LOGIN"
-        Button1.UseVisualStyleBackColor = False
+        Panel2.BackColor = Color.White
+        Panel2.Controls.Add(textIDNumber)
+        Panel2.Location = New Point(50, 168)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(248, 31)
+        Panel2.TabIndex = 2
         ' 
-        ' Label1
+        ' textIDNumber
         ' 
-        Label1.AutoSize = True
-        Label1.Font = New Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        Label1.ForeColor = Color.White
-        Label1.Location = New Point(185, 252)
-        Label1.Name = "Label1"
-        Label1.Size = New Size(113, 17)
-        Label1.TabIndex = 4
-        Label1.Text = "Forget password?"
+        textIDNumber.BorderStyle = BorderStyle.None
+        textIDNumber.Font = New Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        textIDNumber.Location = New Point(10, 5)
+        textIDNumber.Name = "textIDNumber"
+        textIDNumber.Size = New Size(230, 22)
+        textIDNumber.TabIndex = 2
         ' 
-        ' Label2
+        ' PictureBox1
         ' 
-        Label2.AutoSize = True
-        Label2.Font = New Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        Label2.ForeColor = Color.White
-        Label2.Location = New Point(50, 118)
-        Label2.Name = "Label2"
-        Label2.Size = New Size(195, 20)
-        Label2.TabIndex = 5
-        Label2.Text = "Or login with your account"
+        PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), Image)
+        PictureBox1.BackgroundImageLayout = ImageLayout.None
+        PictureBox1.Location = New Point(-19, 48)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(233, 55)
+        PictureBox1.TabIndex = 1
+        PictureBox1.TabStop = False
         ' 
         ' Label3
         ' 
         Label3.AutoSize = True
         Label3.Font = New Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
         Label3.ForeColor = Color.White
-        Label3.Location = New Point(24, 118)
+        Label3.Location = New Point(24, 126)
         Label3.Name = "Label3"
         Label3.Size = New Size(109, 30)
         Label3.TabIndex = 6
         Label3.Text = "Welcome "
         ' 
-        ' Label4
+        ' Label5
         ' 
-        Label4.AutoSize = True
-        Label4.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        Label4.ForeColor = Color.White
-        Label4.Location = New Point(24, 160)
-        Label4.Name = "Label4"
-        Label4.Size = New Size(218, 21)
-        Label4.TabIndex = 6
-        Label4.Text = "Register for a Student account"
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI Semibold", 11F, FontStyle.Bold)
+        Label5.ForeColor = Color.Gray
+        Label5.Location = New Point(24, 160)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(236, 20)
+        Label5.TabIndex = 7
+        Label5.Text = "Register for new EdTech Account."
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.BackColor = Color.FromArgb(CByte(54), CByte(60), CByte(72))
+        TextBox1.BorderStyle = BorderStyle.None
+        TextBox1.Font = New Font("Segoe UI Semibold", 11F, FontStyle.Bold)
+        TextBox1.ForeColor = Color.Gray
+        TextBox1.Location = New Point(24, 194)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(381, 138)
+        TextBox1.TabIndex = 8
+        TextBox1.Text = "Designed to simplify and secure data management, " & vbCrLf & "EdTech Database Manager empowers you to handle " & vbCrLf & "all aspects of data. Register now to streamline your " & vbCrLf & "database management."
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.FromArgb(CByte(161), CByte(0), CByte(0))
+        Button2.FlatAppearance.BorderSize = 0
+        Button2.FlatStyle = FlatStyle.Flat
+        Button2.Font = New Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button2.ForeColor = Color.FromArgb(CByte(224), CByte(224), CByte(224))
+        Button2.Location = New Point(24, 300)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(134, 32)
+        Button2.TabIndex = 10
+        Button2.Text = "REGISTER HERE"
+        Button2.UseVisualStyleBackColor = False
         ' 
         ' Form1
         ' 
@@ -164,7 +193,9 @@ Partial Class Form1
         AutoScaleMode = AutoScaleMode.Font
         BackColor = Color.FromArgb(CByte(54), CByte(60), CByte(72))
         ClientSize = New Size(752, 486)
-        Controls.Add(Label4)
+        Controls.Add(Button2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label5)
         Controls.Add(Label3)
         Controls.Add(PictureBox1)
         Controls.Add(Panel1)
@@ -174,11 +205,11 @@ Partial Class Form1
         Text = "Form1"
         Panel1.ResumeLayout(False)
         Panel1.PerformLayout()
-        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
-        Panel2.ResumeLayout(False)
-        Panel2.PerformLayout()
         Panel3.ResumeLayout(False)
         Panel3.PerformLayout()
+        Panel2.ResumeLayout(False)
+        Panel2.PerformLayout()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
         PerformLayout()
     End Sub
@@ -189,10 +220,12 @@ Partial Class Form1
     Friend WithEvents textIDNumber As TextBox
     Friend WithEvents Panel3 As Panel
     Friend WithEvents txtPassword As TextBox
-    Friend WithEvents Button1 As Button
+    Friend WithEvents btnLogin As Button
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
-    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button2 As Button
 
 End Class
--- Application/EdTech/EdTech/Form1.vb
+++ Application/EdTech/EdTech/Form1.vb
@@ -7,7 +7,58 @@
 
     End Sub
 
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtPassword.TextChanged
 
+
+    Private Sub TextBox1_TextChanged_1(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+    End Sub
+
+    Private Sub textIDNumber_MouseEnter(sender As Object, e As EventArgs) Handles textIDNumber.MouseEnter
+        Dim colorText As Color
+        colorText = Color.FromArgb(54, 60, 72)
+        If textIDNumber.Text = "ID Number" Then
+            textIDNumber.Text = ""
+            textIDNumber.ForeColor = colorText
+        End If
+    End Sub
+
+    Private Sub textIDNumber_MouseLeave(sender As Object, e As EventArgs) Handles textIDNumber.MouseLeave
+        Dim colorHint As Color
+        colorHint = Color.FromArgb(118, 132, 158)
+        If textIDNumber.Text = "" Then
+            textIDNumber.Text = "ID Number"
+            textIDNumber.ForeColor = colorHint
+        End If
+    End Sub
+
+
+    Private Sub txtPassword_MouseEnter(sender As Object, e As EventArgs) Handles txtPassword.MouseEnter
+        Dim colorText As Color
+        colorText = Color.FromArgb(54, 60, 72)
+        If txtPassword.Text = "Password" Then
+            txtPassword.Text = ""
+            txtPassword.PasswordChar = "*"
+            txtPassword.ForeColor = colorText
+        End If
+    End Sub
+
+    Private Sub txtPassword_MouseLeave(sender As Object, e As EventArgs) Handles txtPassword.MouseLeave
+        Dim colorHint As Color
+        colorHint = Color.FromArgb(118, 132, 158)
+        If txtPassword.Text = "" Then
+            txtPassword.Text = "Password"
+            txtPassword.PasswordChar = ""
+            txtPassword.ForeColor = colorHint
+        End If
+    End Sub
+
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        Me.Hide()
+        Dim Form2 As New Form2
+        Form2.Show()
     End Sub
 End Class
